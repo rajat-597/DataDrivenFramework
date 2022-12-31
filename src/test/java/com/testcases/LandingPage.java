d356e47ccb852187bec5447a5a1479c76388fc7b
Rajat <rksahoome140063@gmail.com>
2022-12-31 21:57:01 +0530

    Made some changes in Rana Code

--- src/test/java/com/testcases/LandingPage.java
+++ src/test/java/com/testcases/LandingPage.java
@@ -1,3 +1,9 @@
-This is the new changes done by Rana
-so we need to pill it back to local repository
-to have update code.
+package com.testcases;
+
+public class LandingPage{
+
+//This is the new changes done by Rana
+//so we need to pill it back to local repository
+// to have update code.
+
+}
